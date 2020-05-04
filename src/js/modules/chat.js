2fcbdacebed4838d4b3530034d44413d5ae77c11
Hakan Bilgin <hbi99@hotmail.com>
2020-05-05 01:12:15 +0200

    testing emoticons in chat

--- src/js/modules/chat.js
+++ src/js/modules/chat.js
@@ -5,28 +5,29 @@ const CHAT = {
 		this.transcript = window.find(".transcript");
 		this.input = window.find(".chat-input > div");
 
-		// setTimeout(() => {
-		// 	this.input.html("banan");
-		// 	this.dispatch({ type: "window.keystroke", keyCode: 13 });
-		// }, 1000);
+		setTimeout(() => {
+			this.input.html("hbi :heart: laura :sunglasses:");
+			//this.input.html("Hello :P :* :) :D ;) :( :heart_eyes:");
+			this.dispatch({ type: "window.keystroke", keyCode: 13 });
+		}, 1000);
 	},
 	dispatch(event) {
 		let self = CHAT,
 			name,
-			str,
+			to,
+			msg,
 			el;
 		switch (event.type) {
 			case "window.keystroke":
 				if (event.keyCode === 13) {
-					str = self.input.text();
+					to = "hbi";
+					msg = $.emoticons(self.input.text());
+					console.log(msg);
 
 					name = event.shiftKey ? "received" : "sent";
-					self.transcript.append(`<div class="${name}">${str}</div>`);
+					self.transcript.append(`<div class="${name}">${msg}</div>`);
 
-					defiant.netSend({
-						"to":"hbi",
-						"msg":str
-					});
+					//defiant.netSend({ to, msg });
 
 					// clear input
 					self.input.html("");
