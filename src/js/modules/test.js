b7f20e10abbba865a90694377475a93960b67ee3
Hakan Bilgin <hbi99@hotmail.com>
2023-04-27 13:24:35 +0200

    buggfix

--- src/js/modules/test.js
+++ src/js/modules/test.js
@@ -1,7 +1,11 @@
 
 let Test = {
 	init() {
+		setTimeout(() => this.startUserGame(), 300);
 		// setTimeout(() => this.testFen(), 300);
+	},
+	startUserGame() {
+		chess.els.content.find(`.opt-row .icon-user`).trigger("click");
 	},
 	testFen() {
 		let str = `2kr2nr/pp5p/3R1p2/1p2p3/4P3/8/PPP2PPP/6K1 b - - 0 15`;
